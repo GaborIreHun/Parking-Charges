137b9f5077b30ee54c743237ae679403f5892a23
GaborIreHun <76515173+GaborIreHun@users.noreply.github.com>
2021-01-30 16:47:43 +0000

    Add files via upload

---
+++ ParkingCharges.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class ParkingCharges
+{
+
+    public double minimumCharge = 2;
+    public double parkingLength;
+    public double maximumCharge = 10;
+    public double calculateCharges;
+
+    public void SetParkingLength(double parkingChargesParkingLength)
+    {
+        parkingLength = parkingChargesParkingLength;
+    }
+
+    public double GetParkingLength()
+    {
+        return parkingLength;
+    }
+
+
+    public double CalculateCharges()
+    {
+        if (parkingLength > 0 && parkingLength <= 3)
+        {
+            calculateCharges = minimumCharge;
+            return calculateCharges;
+        }
+        else if (parkingLength > 3 && parkingLength < 19)
+        {
+            calculateCharges = Math.Ceiling(parkingLength) * 0.50 + minimumCharge - 1.5;
+            return calculateCharges;
+        }
+        else if (parkingLength >= 19 && parkingLength <= 24)
+        {
+            calculateCharges = maximumCharge;
+            return calculateCharges;
+        }
+        else if (parkingLength == 0)
+        {
+            calculateCharges = 0;
+            return calculateCharges;
+        }
+        else
+            calculateCharges = maximumCharge + Math.Ceiling(parkingLength) * 0.50 - 12;
+            return calculateCharges;
+    }
+
+}
+
+
+    
+    
+
+
---
+++ ParkingChargesTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+class ParkingChargesTest
+{
+    static void Main()
+    {
+        ParkingCharges parkingCharges = new ParkingCharges();
+
+        Console.WriteLine("\n================== Greetings at the Parking meter ==================");
+
+        double total = 0;
+
+        do
+        {
+            Console.Write("\n\n\nPlease enter the lenght of your parking or 0 to exit: ");
+            double parkingChargestheParkingLength = double.Parse(Console.ReadLine());
+            parkingCharges.SetParkingLength(parkingChargestheParkingLength);
+
+
+            Console.WriteLine($"Your charges for the given parking parking lenght is: {parkingCharges.CalculateCharges()}");
+
+            total += parkingCharges.CalculateCharges();
+
+        } while (parkingCharges.GetParkingLength() != 0);
+
+        Console.WriteLine($"\n\nThe total parking spend is: {total}");
+
+        Console.WriteLine("\n\n====== Thank you and good bye! ======");
+        
+    }
+}
